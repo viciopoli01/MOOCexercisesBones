feca4b6996de276bb1bbcfc88815a081d3324212
viciopoli01 <polivicio@gmail.com>
2020-07-09 18:51:03 +0200

    Using Canny from py file

--- packages/mooc/src/mooc.py
+++ packages/mooc/src/mooc.py
@@ -1,97 +1,34 @@
 #!/usr/bin/env python2
+import sys
+import rospy
+import cv2
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
 
-import io, os, sys, types
-from IPython import get_ipython
-from nbformat import current
-from IPython.core.interactiveshell import InteractiveShell
-
-
-def find_notebook(fullname, path=None):
-    """find a notebook, given its fully qualified name and an optional path
-
-    This turns "foo.bar" into "foo/bar.ipynb"
-    and tries turning "Foo_Bar" into "Foo Bar" if Foo_Bar
-    does not exist.
-    """
-    name = fullname.rsplit('.', 1)[-1]
-    if not path:
-        path = ['']
-    for d in path:
-        nb_path = os.path.join(d, name + ".ipynb")
-        if os.path.isfile(nb_path):
-            return nb_path
-        # let import Notebook_Name find "Notebook Name.ipynb"
-        nb_path = nb_path.replace("_", " ")
-        if os.path.isfile(nb_path):
-            return nb_path
-
-
-class NotebookLoader(object):
-    """Module Loader for Jupyter Notebooks"""
-    def __init__(self, path=None):
-        self.shell = InteractiveShell.instance()
-        self.path = path
-
-    def load_module(self, fullname):
-        """import a notebook as a module"""
-        path = find_notebook(fullname, self.path)
-
-        print ("importing Jupyter notebook from %s" % path)
-
-        # load the notebook object
-        with io.open(path, 'r', encoding='utf-8') as f:
-            nb = current.read(f, 'json')
-
-
-        # create the module and add it to sys.modules
-        # if name in sys.modules:
-        #    return sys.modules[name]
-        mod = types.ModuleType(fullname)
-        mod.__file__ = path
-        mod.__loader__ = self
-        mod.__dict__['get_ipython'] = get_ipython
-        sys.modules[fullname] = mod
-
-        # extra work to ensure that magics that would affect the user_ns
-        # actually affect the notebook module's ns
-        save_user_ns = self.shell.user_ns
-        self.shell.user_ns = mod.__dict__
-
-        try:
-            for cell in nb.worksheets[0].cells:
-                if cell.cell_type == 'code' and cell.language == 'python':
-                    # transform the input to executable Python
-                    code = self.shell.input_transformer_manager.transform_cell(cell.input)
-                    # run the code in themodule
-                    exec(code, mod.__dict__)
-        finally:
-            self.shell.user_ns = save_user_ns
-        return mod
-
-
-class NotebookFinder(object):
-    """Module finder that locates Jupyter Notebooks"""
-    def __init__(self):
-        self.loaders = {}
-
-    def find_module(self, fullname, path=None):
-        nb_path = find_notebook(fullname, path)
-        if not nb_path:
-            return
-
-        key = path
-        if path:
-            # lists aren't hashable
-            key = os.path.sep.join(path)
-
-        if key not in self.loaders:
-            self.loaders[key] = NotebookLoader(path)
-        return self.loaders[key]
-
-sys.meta_path.append(NotebookFinder())
 
 
 import exercise as mooc_exercise
 
+class image_subscriber:
 
-mooc_exercise.sum(5,4)
+  def __init__(self):
+    self.image_pub = rospy.Publisher("/image_subscriber/image_out",Image, queue_size=1)
+
+    self.bridge = CvBridge()
+    self.image_sub = rospy.Subscriber("/camera_node/image/compressed",Image,self.callback, queue_size=1)
+
+  def callback(self,data):
+    try:
+      cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
+    except CvBridgeError as e:
+      print(e)
+
+    self.image_pub.publish(self.bridge.cv2_to_imgmsg(mooc_exercise.CannyF(cv_image), "bgr8"))
+
+def main(args):
+  image_subscriber_node = image_subscriber()
+  rospy.init_node('image_subscriber', anonymous=True)
+  rospy.spin()
+
+if __name__ == '__main__':
+    main(sys.argv)
