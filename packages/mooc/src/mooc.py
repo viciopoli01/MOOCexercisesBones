5e4db9468d9f4a383116c41854de214c12d670e9
viciopoli01 <polivicio@gmail.com>
2020-07-10 11:31:10 +0200

    good

--- packages/mooc/src/mooc.py
+++ packages/mooc/src/mooc.py
@@ -2,33 +2,36 @@
 import sys
 import rospy
 import cv2
-from sensor_msgs.msg import Image
+import os
+from sensor_msgs.msg import CompressedImage
 from cv_bridge import CvBridge, CvBridgeError
 
+from duckietown import DTROS
 
 
 import exercise as mooc_exercise
 
-class image_subscriber:
+class image_subscriber(DTROS):
 
-  def __init__(self):
-    self.image_pub = rospy.Publisher("/image_subscriber/image_out",Image, queue_size=1)
+  def __init__(self,node_name):
+    super(image_subscriber, self).__init__(node_name=node_name)
+    self.veh=os.environ['VEHICLE_NAME'] 
+    camera_topic="/"+self.veh+"/camera_node/image/compressed"
+    out_topic="/"+self.veh+"/mooc/canny/image/compressed"
 
     self.bridge = CvBridge()
-    self.image_sub = rospy.Subscriber("/camera_node/image/compressed",Image,self.callback, queue_size=1)
+
+    self.image_sub = rospy.Subscriber(camera_topic,CompressedImage,self.callback)
+    self.image_pub = rospy.Publisher(out_topic,CompressedImage, queue_size=10)
 
   def callback(self,data):
     try:
-      cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
+      cv_image = self.bridge.compressed_imgmsg_to_cv2(data)
     except CvBridgeError as e:
       print(e)
-
-    self.image_pub.publish(self.bridge.cv2_to_imgmsg(mooc_exercise.CannyF(cv_image), "bgr8"))
-
-def main(args):
-  image_subscriber_node = image_subscriber()
-  rospy.init_node('image_subscriber', anonymous=True)
-  rospy.spin()
+    edges = mooc_exercise.CannyF(cv_image)
+    self.image_pub.publish(self.bridge.cv2_to_compressed_imgmsg(edges))
 
 if __name__ == '__main__':
-    main(sys.argv)
+    image_subscriber_node = image_subscriber("image_subscriber")
+    rospy.spin()
